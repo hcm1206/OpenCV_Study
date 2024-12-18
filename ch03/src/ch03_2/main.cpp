9d93de2ab0adeed6ddf38ab41b9e58ca3d04ca20
hcm1206 <hcm1206@naver.com>
2024-12-18 21:29:08 +0900

    24-12-18 실습 (ch03. Mat 행렬 객체의 복사 및 부분 행렬 추출)

--- ch03/src/ch03_2/main.cpp
+++ ch03/src/ch03_2/main.cpp
@@ -68,8 +68,6 @@ Mat 클래스의 모든 맴버 변수는 public 접근 지시자로 선언되어 있어서 클래스 외부에
 
 /////////////////////////////////////////////////////// (3-7)
 
-// 아래 함수는 Mat 행렬 생성 및 값 초기화만 수행하고 기타 아무런 동작을 하지 않음
-
 void MatOp1() {
 
 	// 비어 있는 행렬을 생성하는 Mat 기본 생성자 
@@ -106,7 +104,97 @@ void MatOp1() {
 	mat5.setTo(1.f);
 }
 
+/////////////////////////////////////////////////////// (3-8)
+
+void MatOp2() {
+	// dog.bmp 강아지 영상을 불러와서 img1에 저장
+	cv::Mat img1 = imread("dog.bmp");
+
+	// 복사 생성자와 대입 연산자를 이용하여 img1의 복사본 영상 img2, img3을 생성(얕은 복사)
+	cv::Mat img2 = img1;
+	cv::Mat img3;
+	img3 = img1;
+
+	// Mat::clone()과 Mat::copyTo() 함수를 이용하여 img1의 복사본 영상 img4, img5를 생성 (깊은 복사)
+	cv::Mat img4 = img1.clone();
+	cv::Mat img5;
+	img1.copyTo(img5);
+
+	// img1 영상의 모든 픽셀을 Scalar(0, 255, 255)에 해당하는 노란색으로 설정
+	img1.setTo(Scalar(0, 255, 255)); // yellow
+
+	// img1~img5 영상을 모두 새 창으로 출력
+	cv::imshow("img1", img1);
+	cv::imshow("img2", img2);
+	cv::imshow("img3", img3);
+	cv::imshow("img4", img4);
+	cv::imshow("img5", img5);
+
+	cv::waitKey();
+	cv::destroyAllWindows();
+}
+
+/////////////////////////////////////////////////////// (3-9)
+
+void MatOp3() {
+	// cat.bmp 고양이 영상을 불러와서 img1에 저장
+	cv::Mat img1 = cv::imread("cat.bmp");
+
+	// 영상을 불러오는데 실패하여 img1 행렬이 비어있다면 오류 메시지 출력 후 종료
+	if (img1.empty()) {
+		cerr << "Image load failed!" << endl;
+		return;
+	}
+
+	// img1 고양이 영상의 특정 사각형 영역 ROI를 설정하여 img2에 저장 (얕은 복사)
+	cv::Mat img2 = img1(Rect(220, 120, 340, 240));
+	// img1 고양의 영상의 특정 사각형 영역 ROI를 설정 후 해당 값을 모두 복사하여 새로운 메모리를 할당하여 img3에 저장 (깊은 복사)
+	cv::Mat img3 = img1(Rect(220, 120, 340, 240)).clone();
+
+	// img2 영상을 색상 반전
+	img2 = ~img2;
+
+	// img1, img2, img3 영상을 모두 새 창으로 출력
+	cv::imshow("img1", img1);
+	cv::imshow("img2", img2);
+	cv::imshow("img3", img3);
+
+	cv::waitKey();
+	cv::destroyAllWindows();
+}
+
 int main() {
+
+	/////////////////////////////////////////////////////// (3-7)
+
+	/*
+	아래 함수는 Mat 행렬 생성 및 값 초기화만 수행하고 기타 아무런 동작을 하지 않음
+	*/ 
+	MatOp1(); 
+
+	/////////////////////////////////////////////////////// (3-8)
+
+	/*
+	아래 함수는 다양한 복사 방법을 이용하여 img1에 저장된 강아지 영상을 새로운 영상 img2, img3, img4, img5에 각각 복사
+	그리고 img1 영상의 모든 픽셀 값을 노란색에 해당하는 Scalar(0, 255, 255)로 설정한 후, 각각의 영상을 화면에 출력
+	*/
+
+	MatOp2();
+
+	/*
+	img1 영상만 노란색으로 설정하였으나 img1, img2, img3 영상이 모두 노란색으로 바뀜
+	img2와 img3 영상이 img1의 픽셀 데이터를 공유하기 때문에 나타난 결과
+	반면에 img4와 img5는 깊은 복사를 수행하였기 때문에 강아지 영상을 그대로 유지
+	*/
+
+	/////////////////////////////////////////////////////// (3-9)
+
+	/*
+	아래 함수는 img1에 저장된 고양이 영상의 부분 영상 영역을 설정하여 새로운 영상 img2, img3에 각각 복사
+	그리고 img2 영상을 색상 반전 후, 각각의 영상을 화면에 출력
+	*/
+
+	MatOp3();
 
 	return 0;
 }
